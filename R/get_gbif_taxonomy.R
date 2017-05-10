0149bf815c9c91bb3cfd7b5bf1692da6825cf130
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-05-10 10:09:58 +0200

    add comments to get_gbif_taxonomy.R

--- R/get_gbif_taxonomy.R
+++ R/get_gbif_taxonomy.R
@@ -1,11 +1,11 @@
-#' Title
+#' Get accepted canonical names and taxonomy for a given species name
 #'
-#' @param x
-#' @param infraspecies
-#' @param fuzzy
-#' @param verbose
+#' @param x a character string or vector of species names.
+#' @param infraspecies logical. If TRUE, the given name is resolved to infraspecies taxon, i.e. subspecies names will not be pooled (not working!)
+#' @param fuzzy logical. If TRUE, function tries fuzzy matching for species requests. May produce output, if normal matching fails. (not working!)
+#' @param verbose logical. If FALSE, warnings and messages are suppressed.
 #'
-#' @return
+#' @return a data.frame of accepted taxon names and higher taxa.
 #' @import taxize
 #' @import data.table
 #' @export
@@ -17,41 +17,45 @@
 
 get_gbif_taxonomy <- function(x, infraspecies = FALSE, fuzzy = FALSE, verbose = TRUE) {
 
-  if(length(x) > 1) {
-    out <-  lapply(x, get_gbif_taxonomy)  # for vectorized input
+  if(length(x) > 1) { # recursive wrapping for vectorized input
+    out <- lapply(x, get_gbif_taxonomy)
     out <- data.table::rbindlist(out, fill = TRUE) # combine into data.frame
 
   } else {
 
     # spellchecking: resolve names using data source 11 (GBIF Backbone Taxonomy)
-
     resolved <- taxize::gnr_resolve(x,
                                     preferred_data_sources = c(11),
                                     best_match_only = TRUE,
                                     canonical = TRUE)
 
+    # return NA for unsuccessful matches
     if(is.null(resolved$matched_name2) ) {
       out <- data.frame(user_supplied_name = x)
       attributes(out)$warning <- paste("No matching species name found!")
     } else {
 
+    # get gbif ID and detailled information, e.g. synonym status
     temp <- taxize::get_gbifid_(resolved$matched_name2)[[1]]
 
+    # switch for allowing for fuzzy matching
     if(!fuzzy) temp <- subset(temp, matchtype == "EXACT")
+
+    # eliminate infraspecies (!! Needs improvement: this will make the function remove supraspecies taxa, too!)
     if(!infraspecies) temp <- subset(temp, rank == "species") #ask for rank according to name resolution provided
 
+    # if given name is a synonym, do a new request for the accepted species name
     if(all(temp$status == "SYNONYM")) {
       if(length(unique(temp$species)) == 1) {
         out <- get_gbif_taxonomy(unique(temp$species))
-        #attributes(out)$warning <- paste("Synonym provided! Automatically set ScientificName to accepted species Name!")
         out$synonym = TRUE
         out$user_supplied_name = x
+
       }
       if(length(unique(temp$species)) > 1) out <- get_gbif_taxonomy(temp$species[which.max(temp$rank)])
-      attributes(out)$warning <- paste("Synonym provided! Automatically set ScientificName to accepted species Name!")
       out$synonym = TRUE
       out$user_supplied_name = x
-    } else {
+    } else {  # if given name is an accepted name, return result into 'out'
       if(any(temp$status == "ACCEPTED")) {
         temp <- subset(temp, status == "ACCEPTED")
         out <- temp
@@ -60,9 +64,12 @@ get_gbif_taxonomy <- function(x, infraspecies = FALSE, fuzzy = FALSE, verbose = 
 
       out <- cbind(user_supplied_name = x, synonym = FALSE,   scientificName = out$species,fullname = out$scientificname, out[,c("rank", "confidence", "kingdom",  "phylum", "class","order", "family", "genus")], taxonomy = "GBIF Backbone Taxonomy",  taxonID = out$usagekey)
 
+      if(out$synonym & verbose) warning(paste("Synonym provided! Automatically set ScientificName to accepted species Name!"))
     }
   }
 
   }
+
+
   return(out)
 }
