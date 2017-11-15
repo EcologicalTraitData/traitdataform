d17fcbcb65a1d4f77ead9b498705535ce2e35d16
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-11-15 16:20:13 +0100

    update as.trait and as.thesaurus function: handle data frames, update terms

--- R/thesaurus.R
+++ R/thesaurus.R
@@ -1,36 +1,70 @@
-#' Create a lookup table of trait definitions (a thesaurus of traits)
-#'
-#' @param x 
-#' @param replace 
-#' @param appliestoall 
-#' @param ... 
-#'
-#' @return a list of formalized objects of class 'trait', as returned by function as.trait().
+#' Create a list of trait definitions (a thesaurus of traits)
 #' 
-#' @details the object class 'trait' comprises necessary information to map a trait name to a  trait definition, a target unit and a globally unique identifier. The thesaurus will be used in function 'standardize.traits()' to apply unit conversion and factor level harmonization. 
+#' Create a trait thesaurus object for use as a reference object/lookup table of
+#' traits within function `standardize.traits()`.
 #' 
+#' @param ... multiple objects of class 'trait' (produced by function
+#'   `as.trait()`) or a data.frame containing columns according to the terms
+#'   provided by https://ecologicaltraitdata.github.io/ETS/#terms-for-thesauri.
+#' @replace named character vector, with new names as values, and old names as names.
+#'   
+#' @return a list of formalized objects of class 'trait', as returned by
+#'   function as.trait().
+#'   
+#' @details the object class 'trait' comprises necessary information to map a
+#'   trait name to a  trait definition, a target unit and a globally unique
+#'   identifier. The thesaurus will be used in function 'standardize.traits()'
+#'   to apply unit conversion and factor level harmonization.
+#'   
 #' @export
-#'
+#' @import plyr
+#' 
 #' @examples
 #' 
-#' as.thesaurus(body_length = as.trait("body_length", traitUnit = "mm", traitUnitStd = "mm"), 
-#'              antenna_length = as.trait("antenna_length", traitUnitStd = "mm"),
-#'              metafemur_length = as.trait("metafemur_length"),
-#'              eyewidth = as.trait("eyewidth_corr")
+#' # provide traitlist by defining individual traits using function `as.trait()`:
+#' 
+#' traitlist <- as.thesaurus(body_length = as.trait("body_length", expectedUnit = "mm", 
+#'     valueType = "numeric", 
+#'     identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Body_length"), 
+#'   antenna_length = as.trait("antenna_length", expectedUnit = "mm", 
+#'     valueType = "numeric", 
+#'     identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Antenna_length"),
+#'   metafemur_length = as.trait("metafemur_length", expectedUnit = "mm", 
+#'     valueType = "numeric", 
+#'     identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Femur_length")
 #' )
 #' 
+#' # provide traitlist from data frame object: 
+#' 
 #' as.thesaurus(data.frame(
-#'   traitName = c("body_length", "antenna_length", "metafemur_length", "eyewidth_corr"),
-#'   # traitNameStd = traitName,
-#'   traitUnit = "mm",
-#'   traitType = "numeric",
-#'   traitDescription = c("body length in mm", "length of antenna in mm", "length of metafemur in mm", "eye width in mm")
+#'   trait = c("body_length", "antenna_length", "metafemur_length", "eyewidth_corr"),
+#'   expectedUnit = "mm",
+#'   valueType = "numeric",
+#'   traitDescription = c("body length in mm", "length of antenna in mm", "length of metafemur in mm", "eye width in mm"), 
+#'   identifier = c("http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Body_length", 
+#'     "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Antenna_length", 
+#'     "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Femur_length", 
+#'     "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Eye_diameter"
+#'     )
 #' )
+#' )
+#' 
+#' # provide traitlist from remote archive with renaming
+#' 
+#' traits1 <- as.thesaurus(read.csv("https://raw.githubusercontent.com/EcologicalTraitData/TraitDataList/master/traitdatastandard_traitlist.csv"), 
+#' replace = c(traitID = "identifier", 
+#'             traitName = "trait",
+#'             traitUnit = "expectedUnit",
+#'             Comments = "comments") 
 #' )
 
-as.thesaurus <- function(...) {
+as.thesaurus <- function(...,
+                         replace = NULL
+                         ) {
   if( "data.frame" %in% class(..1)) {
-    out <- lapply(split(..1, f = ..1$traitName, drop= TRUE), function(y) do.call(as.trait, y))
+    input <- ..1
+    if(!is.null(replace)) input <- plyr::rename(input, replace)
+    out <- lapply(split(input, f = input$trait, drop= TRUE), function(y) do.call(as.trait, y))
     
   }
   if("trait" %in% class(..1)) {
@@ -42,55 +76,86 @@ as.thesaurus <- function(...) {
   return(out)
 }
 
+#' @export
+print.thesaurus <- function(x) {
+  out <- do.call(rbind, lapply(x,data.frame))
+  print(out)
+}
+
 #' Create a trait definition
+#' 
+#' Creating an object containing a standardised trait definition according to the Ecological Traitdata Standard. Parameters correspond to the definition at http://ecologicaltraitdata.github.io/ETS/#terms-for-thesauri. 
 #'
-#' @param traitName 
-#' @param traitID 
-#' @param author 
-#' @param URI 
-#' @param traitUnit 
-#' @param factorLevels 
-#' @param traitDescription 
-#' @param bibliographicReference 
+#' @param trait A character string, providing an intuitive, human-readable trait name.
+#' @param identifier Unique identifier for the trait, ideally unique and stable URI which identify the source of the trait definition.
+#' @param traitDescription A short, unambiguous definition of the trait. May refer to a method of measurement.
+#' @param comments Details and Examples for clarification of the trait definition.
+#' @param broaderTerm One or several terms that enclose the trait definition. 
+#' @param narrowerTerm One or several terms that are enclosed by the trait definition. 
+#' @param expectedUnit the unit expected for measurement entries. 
+#' @param maxAllowedValue An upper boundary for accepted numerical values.
+#' @param minAllowedValue A lower boundary for accepted numerical values.
+#' @param factorLevels A comma separated list of terms comprising the constrained vocabulary for categorical traits or ordinal binary traits.
+#' @param valueType the type of trait values. Possible entries are 'numeric', 'integer', 'categorical', 'logical', or 'character'.
+#' @param source 
+#' @param relationSource
 #' @param version 
+#' @param author 
 #' @param ... 
 #'
-#' @returnh
+#' @return 
 #' @export
 #'
 #' @examples
 #' 
-#' body_size = as.trait("body_size", traitID = "http://url.to/thesaurus.html#body_size", author = "F.D. Schneider", traitDescription = "body length from head  to end of abdomen in mm")
+#' body_length <- as.trait("body_length", expectedUnit = "mm", valueType = "numeric", 
+#'   Identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Body_length", 
+#'   traitDescription = "The known longest dimension of the physical structure of organisms", relationSource = "Maggenti and Maggenti, 2005", broaderTerm = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Body_dimension")
 #' 
 as.trait <- function(
-  traitName,
-  traitID = NA,
-  author = NA,
-  URI = NA,
-  traitUnit = NA,
-  traitUnitStd = traitUnit,
+  trait,
+  identifier = NA,
+  broaderTerm = NA,
+  narrowerTerm = NA,
+  expectedUnit = NA,
+  maxAllowedValue = NA,
+  minAllowedValue = NA,
   factorLevels = NA,
-  traitType = NA,
+  valueType = NA,
   traitDescription = NA,
-  bibliographicReference = NA,
+  comments = NA,
+  relationSource = NA,
+  source = NA,
   version = NA,
+  author = NA,
   ...
 ) {
-  out <- list(traitName = traitName,
-              traitID = traitID,
-              author = author,
-              URI = URI,
-              traitUnit = traitUnit,
-              traitUnitStd = traitUnitStd,
+  out <- list(trait = trait,
+              identifier = identifier,
+              broaderTerm = broaderTerm,
+              narrowerTerm = narrowerTerm,
+              expectedUnit = expectedUnit,
+              maxAllowedValue = maxAllowedValue,
+              minAllowedValue = minAllowedValue,
               factorLevels = factorLevels,
-              traitType = traitType,
+              valueType = valueType,
               traitDescription = traitDescription,
-              bibliographicReference = bibliographicReference,
-              version = version
+              comments = comments,
+              relationSource = relationSource,
+              source = source,
+              version = version,
+              author = author
               )
   class(out) <- c("trait", "list")
   return(out)
 }
+
+#' @export
+print.trait <- function(x) {
+  out <- do.call(rbind, lapply(x,data.frame))
+  print(out)
+}
+
 
 #print.trait <- function(x) {
 #  cat("traitName:")
