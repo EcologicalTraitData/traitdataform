481350c9b77438f61cf949e298b713c76567ca78
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-11-20 10:51:11 +0100

    update mutate method: fix dimension attributes, apply computations correctly

--- R/mutate.traitdata.R
+++ R/mutate.traitdata.R
@@ -1,17 +1,24 @@
 #' Mutate traits within a traitdata object.
 #' 
-#' This function allows to transform, factorize, or combine trait measurements
+#' This function allows to transform, factorize, or combine trait measurements 
 #' into compound measurements or update factor levels into binaries.
 #' 
 #' @param .data the traitdata object to transform
 #' @param ... named parameters giving definitions of new columns.
 #'   
-#' @return an updated traitdata object with the new trait measures or facts
-#'   appended to the original table. If the given trait name has been refined,
+#' @return an updated traitdata object with the new trait measures or facts 
+#'   appended to the original table. If the given trait name has been refined, 
 #'   it will be replaced.
 #'   
-#' @details Note that units will not be maintained or refined. They can be
-#'   assigned later through the `thesaurus()` function.
+#' @details Note that units for numerical traits will not be maintained or 
+#'   computed (e.g. for ratios). They should be assigned later through the 
+#'   `thesaurus()` function.
+#'   
+#'   It is advised to mutate traits before applying `standardize.traits()`! If 
+#'   the mutate function is applied to a standardised dataset, the new trait 
+#'   will not be mapped automatically to the provided thesaurus. (automated
+#'   re-mapping might be added in later versions of the package.)
+#'   
 #' @export
 #' 
 #' @examples
@@ -27,8 +34,19 @@
 #' head(dataset3)
 #' 
 #' updated <- mutate.traitdata(dataset3, predator = Feeding_guild == "c" )
-#' head(updated)
+#' head(updated[updated$traitName == "predator",])
 #' levels(updated$traitName)
+#' 
+#' 
+#' ## 
+#' 
+#' 
+#' updated <- mutate.traitdata(dataset2, 
+#'                             Body_shape = Body_length/Body_width, 
+#'                             Body_volume = Body_length*Body_width*Body_height,
+#'                             Wingload = Wing_length*Wing_width/Body_volume)
+#' 
+#' head(updated[updated$traitName %in% c("Body_shape", "Body_volume", "Wingload"),])
 
 
 mutate.traitdata <- function(.data, 
@@ -36,39 +54,38 @@ mutate.traitdata <- function(.data,
                              ) {
   stopifnot(is.data.frame(.data) || is.list(.data) || is.environment(.data))
   
-  columns <- names(.data[,-which(names(.data) %in% c("traitName", "traitValue", "traitUnit"))])
+  in_traits <- levels(.data$traitName)
+  temp <- cast.traitdata(.data)
   
-  #if no occurence ID is provided
-  temp <- reshape::cast(.data, eval(expression(paste(paste(columns, collapse =" + "), "~ traitName"))), value = 'traitValue') 
-  # else group on level of occurences
-  
+  # extract mutations to apply into list
   out_traits <- as.list(substitute(list(...))[-1])
   out_traits <- out_traits[names(out_traits) != ""]
   
+  # apply each list entry to matrix
   for (col in names(out_traits)) {
-    units <- 
+     
     temp[[col]] <- eval(out_traits[[col]], temp, parent.frame())
   }
   class(temp) <- "data.frame"
   
   out <- reshape::melt(temp, 
                        measure.vars = names(out_traits), 
-                       id.vars = c("scientificName", 
-                                   c("occurenceID")["occurenceID" %in% names(temp)],
-                                   c("measurementID")["measurementID" %in% names(temp)]), 
-                       variable_name = "traitName"
+                       id.vars = names(temp[,
+                            which(!names(temp) %in% c(in_traits,names(out_traits)))]), 
+                       variable_name = "traitName",
+                       na.rm = TRUE
                        ) 
   
   names(out)[names(out) == "value"] <- "traitValue"
-  row.names(out) <- seq_along(out[,1])
-  
-  # sort columns according to glossary of terms
-  out <- out[, order(match(names(out), glossary$columnName) )]
-  
+
   out <- plyr::rbind.fill(subset(.data, !traitName %in% names(out_traits)), out)
+
+    # sort columns according to glossary of terms
+  out <- out[, order(match(names(out), glossary$columnName) )]
   
   attribs <- attributes(.data)
   attribs$names <- attributes(out)$names
+  attribs$row.names <- seq_along(out[,1])
   attributes(out) <- attribs
   
   return(out)
