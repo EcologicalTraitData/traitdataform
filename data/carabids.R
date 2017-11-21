f498f545d33b8d79f2915a0012e11d83946c8d5a
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-11-21 21:50:45 +0100

    refine data object carabids: add metadata and thesaurus as attributes

--- data/carabids.R
+++ data/carabids.R
@@ -1,16 +1,40 @@
 # for roxygen2 documentation please edit file R/data.R!
 
-carabids <- utils::read.table("http://datadryad.org/bitstream/handle/10255/dryad.134418/carabid%20traits%20final.txt", sep = "\t", header = TRUE)
+carabids <- utils::read.table("http://datadryad.org/bitstream/handle/10255/dryad.134418/carabid%20traits%20final.txt", 
+                              sep = "\t", 
+                              header = TRUE,
+                              fileEncoding = "UTF-8"
+                              )
 
-attr(carabids, 'citeAs') <- utils::bibentry(
-  bibtype = "Article",
-  title = "Sensitivity of functional diversity metrics to sampling intensity",
-  journal = "Methods in Ecology and Evolution",
-  author = c(utils::as.person("Fons van der Plas, Roel van Klink, Pete Manning, Han Olff, Markus Fischer")
-  ),
-  year = 2017,
-  doi = "10.1111/2041-210x.12728"
-)
+attr(carabids, 'metadata') <- as.metadata(
+      datasetName = "Carabid traits",
+      datasetID = "carabids",
+      bibliographicCitation =  utils::bibentry(
+        bibtype = "Article",
+        title = "Sensitivity of functional diversity metrics to sampling intensity",
+        journal = "Methods in Ecology and Evolution",
+        author = c(utils::as.person("Fons van der Plas, Roel van Klink, Pete Manning, Han Olff, Markus Fischer")
+        ),
+        year = 2017,
+        doi = "10.1111/2041-210x.12728"
+      ),
+      author = "Fons van der Plas", 
+      license = "http://creativecommons.org/publicdomain/zero/1.0/"
+       )
 
-print({cat("loading dataset 'carabids' from original data source! \n When using this data, please cite the original publication: \n") 
-  (attributes(carabids)$citeAs) })
+attr(carabids, 'thesaurus') <-  as.thesaurus(
+          body_length = as.trait("body_length", 
+                              expectedUnit = "mm", valueType = "numeric", 
+                              identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Body_length"), 
+          antenna_length = as.trait("antenna_length", 
+                              expectedUnit = "mm", valueType = "numeric", 
+                              identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Antenna_length"),
+          metafemur_length = as.trait("femur_length", 
+                              expectedUnit = "mm", valueType = "numeric", 
+                              identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Femur_length"),
+          eyewidth_corr = as.trait("eye_diameter", 
+                              expectedUnit = "mm", valueType = "numeric",
+                              identifier = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Eye_diameter")
+        )
+
+print(attributes(carabids)$metadata)
