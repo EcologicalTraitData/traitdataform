36a0f639f185c81c0cc00cd0f12a318a7f2372b1
caterinap <caterina.penone@gmail.com>
2017-11-22 19:10:11 +0100

    Updated mammaldiet

--- data/mammaldiet.R
+++ data/mammaldiet.R
@@ -3,19 +3,27 @@
 mammaldiet <- utils::read.csv("http://datadryad.org/bitstream/handle/10255/dryad.64565/MammalDIET_v1.0.txt?sequence=1",
                               sep = "\t",
                               fileEncoding = "UTF-8"
-                              )
-
-attr(mammaldiet, 'citeAs') <- utils::bibentry(
-  bibtype = "Article",
-  title = "Establishing macroecological trait datasets: digitalization, extrapolation, and validation of diet preferences in terrestrial mammals worldwide",
-  journal = "Ecology and Evolution",
-  volume = 4,
-  issue = 12,
-  pages = 2913-2930,
-  author = c(utils::person(given = "Wilm Daniel", family = "Kissling", email = "danielkissling@web.de")
-  ),
-  year = 2014,
-  doi = "10.1002/ece3.1136"
 )
-print({cat("loading dataset 'mammaldiet' from original data source! \n When using this data, please cite the original publication: \n") 
-  (attributes(mammaldiet)$citeAs) })
+
+attr(mammaldiet, 'metadata') <- traitdataform::as.metadata(
+  datasetName = "Mammal diet database",
+  datasetID = "mammaldiet",
+  bibliographicCitation =  utils::bibentry(
+    bibtype = "Article",
+    title = "Establishing macroecological trait datasets: digitalization, extrapolation, and validation of diet preferences in terrestrial mammals worldwide",
+    journal = "Ecology and Evolution",
+    author = c(utils::as.person("Wilm Daniel Kissling, Lars Dalby, Camilla Fløjgaard, Jonathan Lenoir, Brody Sandel,
+                                Christopher Sandom, Kristian Trøjelsgaard, Jens-Christian Svenning")),
+    year = 2014,
+    doi = "10.1002/ece3.1136"
+  ),
+  author = "Wilm Daniel Kissling",
+  license = "http://creativecommons.org/publicdomain/zero/1.0/"
+)
+
+attr(mammaldiet, 'thesaurus') <-  traitdataform::as.thesaurus(
+  diet_content = traitdataform::as.trait("diet_content", valueType = "factor"),
+  TrophicLevel = traitdataform::as.trait("TrophicLevel", valueType = "factor"),
+  dietary_guild = traitdataform::as.trait("dietary_guild", valueType = "factor"))
+
+print(attributes(mammaldiet)$metadata)
