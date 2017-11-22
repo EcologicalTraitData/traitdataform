836ad23454e49f0ab3fae7e613ba58b22f13dd07
caterinap <caterina.penone@gmail.com>
2017-11-22 19:23:12 +0100

    Updated mammaldiet

--- data/mammaldiet.R
+++ data/mammaldiet.R
@@ -16,14 +16,36 @@ attr(mammaldiet, 'metadata') <- traitdataform::as.metadata(
                                 Christopher Sandom, Kristian Trøjelsgaard, Jens-Christian Svenning")),
     year = 2014,
     doi = "10.1002/ece3.1136"
-  ),
+    ),
   author = "Wilm Daniel Kissling",
   license = "http://creativecommons.org/publicdomain/zero/1.0/"
 )
 
 attr(mammaldiet, 'thesaurus') <-  traitdataform::as.thesaurus(
-  diet_content = traitdataform::as.trait("diet_content", valueType = "factor"),
+  diet_Animal = traitdataform::as.trait("diet_Animal", valueType = "integer"),
+  diet_Vertebrate = traitdataform::as.trait("diet_Vertebrate", valueType = "integer"),
+  diet_Mammal = traitdataform::as.trait("diet_Mammal", valueType = "integer"),
+  diet_Bird = traitdataform::as.trait("diet_Bird", valueType = "integer"),
+  diet_Herptile = traitdataform::as.trait("diet_Herptile", valueType = "integer"),
+  diet_Fish = traitdataform::as.trait("diet_Fish", valueType = "integer"),
+  diet_Invertebrate = traitdataform::as.trait("diet_Invertebrate", valueType = "integer"),
+  diet_Plant = traitdataform::as.trait("diet_Plant", valueType = "integer"),
+  diet_Seed = traitdataform::as.trait("diet_Seed", valueType = "integer"),
+  diet_Fruit = traitdataform::as.trait("diet_Fruit", valueType = "integer"),
+  diet_Nectar = traitdataform::as.trait("diet_Nectar", valueType = "integer"),
+  diet_Root = traitdataform::as.trait("diet_Root", valueType = "integer"),
+  diet_Leaf = traitdataform::as.trait("diet_Leaf", valueType = "integer"),
+  diet_Woody = traitdataform::as.trait("diet_Woody", valueType = "integer"),
+  diet_Herbaceous = traitdataform::as.trait("diet_Herbaceous", valueType = "integer"),
+  diet_Other = traitdataform::as.trait("diet_Other", valueType = "integer"),
+  
   TrophicLevel = traitdataform::as.trait("TrophicLevel", valueType = "factor"),
-  dietary_guild = traitdataform::as.trait("dietary_guild", valueType = "factor"))
+  
+  diet_guild_MammalEater = traitdataform::as.trait("diet_guild_MammalEater", valueType = "logical"),
+  diet_guild_Insectivore = traitdataform::as.trait("diet_guild_Insectivore", valueType = "logical"),
+  diet_guild_Frugivore = traitdataform::as.trait("diet_guild_Frugivore", valueType = "logical"),
+  diet_guild_Granivore = traitdataform::as.trait("diet_guild_Granivore", valueType = "logical"),
+  diet_guild_Folivore = traitdataform::as.trait("diet_guild_Folivore", valueType = "logical")
+)
 
 print(attributes(mammaldiet)$metadata)
