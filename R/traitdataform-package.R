adf23de146a219fdd99b7155aa2451116d121f45
Florian Schneider <florian.schneider@univ-montp2.fr>
2018-11-19 13:01:34 +0100

    implement codemeta suggestions

---
+++ R/traitdataform-package.R
@@ -0,0 +1,26 @@
+#' Package 'traitdataform': harmonizing ecological trait data in R
+#'
+#' This package assists in handling functional trait data and transferring them
+#' into the Trait Data Standard (Schneider et al. in preparation).
+#'
+#' There are two major use cases for the package:
+#'
+#' - preparation of own trait datasets for upload into public data bases, and 
+#' - harmonizing trait datasets from different sources by moulding them into a
+#' unified format.
+#'
+#' The toolset of the package includes
+#'
+#' - transforming species-trait-matrix or occurence table data into a unified
+#' long-table format 
+#' - mapping column names into terms provided in a standard trait vocabulary 
+#' - matching of species names into GBIF Backbone Taxonomy (taxonomic ontology 
+#' server) 
+#' - matching of trait names into a user-provided traitlist, i.e. a thesaurus 
+#' of traits 
+#' - unifying trait values into target unit format and legit factor levels 
+#' - saving trait dataset into a desired format using templates (e.g. for BExIS)
+#' 
+#' @keywords internal
+#' @name traitdataform-package
+NULL
