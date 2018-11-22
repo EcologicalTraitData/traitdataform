0991074b1a70c4ba54504e05490bc386f9b3ca4a
Florian Schneider <florian.schneider@univ-montp2.fr>
2018-11-22 21:37:47 +0100

    add tests for data integrity

--- tests/testthat/test.columns_to_ETS.R
+++ tests/testthat/test.columns_to_ETS.R
@@ -1,7 +1,10 @@
 context("trait data structure")
 library(traitdataform)
 
+pulldata('heteroptera_raw')
 pulldata('arthropodtraits')
+pulldata('carabids')
+
 
 test_that("keep matrix structure", {
   
@@ -23,13 +26,21 @@ test_that("reformat, with trait columns provided as argument", {
   expect_length(dd1[1,], 5)
   expect_s3_class(dd1, "traitdata")
   
+  dd2 <- as.traitdata(carabids, traits = c("body_length", "antenna_length", "metafemur_length", "eyewidth_corr"))
+  dd2 <- as.traitdata(carabids)
+   
+  expect_length(dd2$scientificName,480)
+  expect_length(dd2[1,], 7)
+  expect_s3_class(dd2, "traitdata")
   
 } )
 
 test_that("reformat species data, with trait columns provided as attributes", {
-  
+
   expect_message(as.traitdata(arthropodtraits), "Input is taken to be a species -- trait matrix")
   dd1 <- as.traitdata(arthropodtraits)
+  
+  expect_true(arthropodtraits[1065,"Author"] == "(Herrich-Schäffer, 1841)")
   
   expect_length(dd1$scientificName,10238)
   expect_length(dd1[1,], 5)
@@ -39,17 +50,26 @@ test_that("reformat species data, with trait columns provided as attributes", {
 })
 
  
-# test_that("reformat observation data, with trait columns provided as attributes", {
-#   
-#   pulldata('heteroptera_raw')
-#   
-#   dd2 <- as.traitdata(heteroptera_raw)
-# 
-#   expect_length(dd2$scientificName, 9386)
-#   expect_length(dd2[1,], 10)
-#   expect_s3_class(dd2, "traitdata")
-# 
-# })
+test_that("reformat observation data, with trait columns provided as attributes", {
+
+  expect_true(exists("heteroptera_raw"))
+  
+  expect_true(heteroptera_raw[1,6] == "(Fallen, 1807)")
+  expect_true(heteroptera_raw[13,6] == "(Herrich-Schäffer, 1841)")
+  
+  expect_false(is.null(attributes(heteroptera_raw)$taxa))
+  expect_true(attributes(heteroptera_raw)$taxa == "SpeciesID")
+  
+  expect_false(is.null(attributes(heteroptera_raw)$thesaurus))
+  expect_s3_class(attributes(heteroptera_raw)$thesaurus, "thesaurus")
+
+  dd3 <- as.traitdata(heteroptera_raw)
+
+  expect_length(dd3$scientificName, 9386)
+  expect_length(dd3[1,], 10)
+  expect_s3_class(dd3, "traitdata")
+
+})
 
 test_that("reformat, with trait columns provided as thesaurus", {
   
@@ -62,18 +82,31 @@ test_that("reformat, with trait columns provided as thesaurus", {
   
 })
 
-# 
-# 
-# test_that("reformat, with taxa column provided as argument", {
-#   
-#   
-# }
-# )
-# 
-# 
-# test_that("reformat, with taxa column provided as argument", {
-#   
-#   
-# }
-# )
-# 
+
+
+test_that("reformat, with 'keep' argument", {
+
+  dd2 <- as.traitdata(carabids, traits = c("body_length", "antenna_length", "metafemur_length", "eyewidth_corr"), keep = c(measurementDeterminedBy = "source_measurement", measurementRemarks = "note"))
+
+  expect_length(dd2$scientificName,480)
+  expect_length(dd2[1,], 7)
+  expect_true( all(names(dd2) ==  c("scientificName", "traitName", "traitValue", "traitUnit", "measurementID", "measurementDeterminedBy", "measurementRemarks")) )
+  expect_s3_class(dd2, "traitdata")
+
+
+}
+)
+
+
+test_that("reformat, with 'drop' argument", {
+
+  dd2 <- as.traitdata(carabids, traits = c("body_length", "antenna_length", "metafemur_length", "eyewidth_corr"), keep = c(measurementDeterminedBy = "source_measurement"), drop = c("note"))
+
+  expect_length(dd2$scientificName,480)
+  expect_length(dd2[1,], 6)
+  expect_true( all(names(dd2) ==  c("scientificName", "traitName", "traitValue", "traitUnit", "measurementID", "measurementDeterminedBy")) )
+  expect_s3_class(dd2, "traitdata")
+
+}
+)
+
