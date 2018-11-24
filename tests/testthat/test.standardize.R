ab9d3e35dd3df85abbb5fc1e3f5848601ae79e66
Florian Schneider <florian.schneider@univ-montp2.fr>
2018-11-24 11:40:24 +0100

    bounce to v0.3.4, build doc pages

--- tests/testthat/test.standardize.R
+++ tests/testthat/test.standardize.R
@@ -26,5 +26,60 @@ test_that("mapping of taxa works", {
 
 
 test_that("mapping of traits based on thesaurus works", {
+  pulldata("heteroptera_raw")
   
+  dataset2 <- as.traitdata(heteroptera_raw)
+                            
+                          
+  traits2 <- as.thesaurus(
+     Body_length = as.trait("Body_length",
+                            expectedUnit = "mm", valueType = "numeric",
+                            traitDescription = "From the tip of the head to the end of the abdomen"),
+     Antenna_Seg1 = as.trait("Antenna_Seg1",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of first antenna segment",
+                             broaderTerm = "http://ecologicaltraitdata.github.io/TraitDataList/Antenna_length"),
+     Antenna_Seg2 = as.trait("Antenna_Seg2",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of second antenna segment",
+                             broaderTerm = "http://ecologicaltraitdata.github.io/TraitDataList/Antenna_length"),
+     Antenna_Seg3 = as.trait("Antenna_Seg3",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of third antenna segment",
+                             broaderTerm = "http://ecologicaltraitdata.github.io/TraitDataList/Antenna_length"),
+     Antenna_Seg4 = as.trait("Antenna_Seg4",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of fourth antenna segment",
+                             broaderTerm = "http://ecologicaltraitdata.github.io/TraitDataList/Antenna_length"),
+     Antenna_Seg5 = as.trait("Antenna_Seg5",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of fifth antenna segment (only Pentatomoidea)",
+                             broaderTerm = "http://ecologicaltraitdata.github.io/TraitDataList/Antenna_length"),
+     Hind.Femur_length = as.trait("Hind.Femur_length",
+                             expectedUnit = "mm", valueType = "numeric",
+                             traitDescription = "Length of the femur of the hind leg",
+                             broaderTerm = "http://t-sita.cesab.org/BETSI_vizInfo.jsp?trait=Femur_length")
+   )
+                           
+   dataset2Std <- standardize_traits(dataset2, 
+                                thesaurus = traits2
+                                )
+   expect_equal(dim(dataset2Std), c(2586,13))
+})
+
+
+test_that("unit conversion works", {
+  
+  pulldata("carabids")
+  
+})
+
+test_that("factor level harmonizationworks", {
+  
+  pulldata("arthropodtraits")
+  dd1 <- as.traitdata(arthropodtraits, traits = c("Body_Size", "Dispersal_ability", "Feeding_guild_short", "Feeding_mode"))
+  dd1Std <- standardize_traits(dd1)
+  
+  expect_equal(dim(dd1Std), c(4920,9))
+  expect_equal(sum(subset(dd1Std, traitName == "Body_Size")$traitValueStd), 5795.08)
 })
