e518ad5549e8d91598d8493714044bc3e78d402f
Florian Schneider <florian.schneider@univ-montp2.fr>
2018-11-27 22:47:11 +0100

    make glossary internal

--- R/glossary.R
+++ R/glossary.R
@@ -1,7 +1,6 @@
 #' Ecological Trait-data Standard vocabulary (ETS)
 #' 
 #' @description The terms and concepts as defined by the ETS (https://terminologies.gfbio.org/terms/ets/pages/)
-#' @export 
 "glossary"
 
 glossary <- read.csv("https://raw.githubusercontent.com/EcologicalTraitData/TraitDataStandard/master/ETS.csv")
